9c4cd5af9f4ff5fb9e37d195708acb94f3cff2e3
Tharindu Thennakoon <chanaka152@gmail.com>
2023-08-22 09:48:27 +0530

    Exit button

--- src/main/java/lk/ijse/dep11/AppInitializer.java
+++ src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class AppInitializer extends Application {
     public static void main(String[] args) {
@@ -12,6 +15,13 @@ public class AppInitializer extends Application {
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
+//        do{
+//            System.out.println("App started");
+//            splashScreen(primaryStage);
+//            Thread.sleep(2000);
+//            break;
+//        }while (true);
+
         AnchorPane root = FXMLLoader.load(getClass().getResource("/view/TextEditor.fxml"));
         Scene mainScene = new Scene(root);
         primaryStage.setScene(mainScene);
@@ -19,4 +29,16 @@ public class AppInitializer extends Application {
         primaryStage.centerOnScreen();
         primaryStage.show();
     }
+
+//    private void splashScreen (Stage primaryStage) throws Exception{
+//        AnchorPane transRoot= FXMLLoader.load(getClass().getResource("/view/SplashScreen.fxml"));
+//        Scene mainScene=new Scene(transRoot);
+//        primaryStage.setScene(mainScene);
+//        primaryStage.setTitle("Simple Text Editor");
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+//        transRoot.setBackground(Background.fill(Color.TRANSPARENT));
+//        mainScene.setFill(Color.TRANSPARENT);
+//        primaryStage.centerOnScreen();
+//        primaryStage.show();
+//    }
 }
--- src/main/java/lk/ijse/dep11/TextEditorController.java
+++ src/main/java/lk/ijse/dep11/TextEditorController.java
@@ -1,4 +1,18 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 public class TextEditorController {
+    public MenuItem btnExit;
+    public AnchorPane root;
+
+    public void btnExitOnAction(ActionEvent e) {
+//        Stage primaryStage = (Stage) root.getScene().getWindow();
+//        primaryStage.close();
+        System.exit(0);
+    }
 }
